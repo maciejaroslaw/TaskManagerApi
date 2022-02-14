7697e9837fe7f7895cab081469b68e24d5aaa2d0
unknown <maciejaroslaw@gmail.com>
2022-02-14 22:52:32 +0100

    api test

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const userRoutes = require('./routes/user.routes');
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin: "http://localhost:8080"}))
+app.use(cors({origin: "http://localhost:8080"}));
+
+app.get('/', (req, res)=>{
+    res.send("Działam");
+})
 
 app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
