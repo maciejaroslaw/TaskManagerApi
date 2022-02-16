e46142cc775fd3132dcb10e85ec944aafebccdf6
maciejaroslaw <48844889+maciejaroslaw@users.noreply.github.com>
2022-02-16 13:37:55 +0100

    fix cors

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const userRoutes = require('./routes/user.routes');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://620cebd0718e4c000837af0e--adoring-banach-08ca3c.netlify.app',
+    optionsSuccessStatus: 200
+}));
 
 app.get('/', (req, res)=>{
     res.send("Działam");
