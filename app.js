19529b1ecf9129293e5dc30559845681a9c64c09
maciejaroslaw <48844889+maciejaroslaw@users.noreply.github.com>
2022-02-16 10:54:39 +0100

    enable cors from all origins

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const userRoutes = require('./routes/user.routes');
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin: "http://localhost:8080"}));
+app.use(cors());
 
 app.get('/', (req, res)=>{
     res.send("Działam");
